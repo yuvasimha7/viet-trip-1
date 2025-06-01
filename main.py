8e706b3fb3e05c321a7391eaad33724608c6bf95
yuva simha <yuva.simha7@gmail.com>
2025-06-01 10:25:17 +0530

    1.0.1 make railway database work

--- main.py
+++ main.py
@@ -36,8 +36,12 @@ if __name__ == "__main__":
 import os
 from flask import Flask, render_template
 from dotenv import load_dotenv 
+from app import create_app,db 
+
 
 load_dotenv()
+
+
 
 try:
     from app import create_app
@@ -61,6 +65,9 @@ else:
         def home():
             return render_template('index.html')
 
+app = create_app()
+with app.app_context():
+    db.create_all()
 
 # Optional: Only needed if running locally with python main.py
 if __name__ == "__main__":
